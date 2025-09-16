d0b783c641a563e621d04e0eda911c1d1803a531
bigyambat <bigyambat@gmail.com>
2025-09-16 10:09:27 -0700

    Remove large tools directory and add .gitignore

--- tools/cellranger-9.0.1/lib/python/cellranger/targeted/targeted_spatial.py
+++
@@ -1,5 +0,0 @@
-#
-# Copyright (c) 2020 10X Genomics, Inc. All rights reserved.
-#
-"""Constants and methods only applicable to spatial targeting."""
-SPATIAL_TARGET_DISALLOWED_PANEL_TYPES = ["fully_custom"]
--- tools/cellranger-9.0.1/lib/python/tenkit/__init__.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env python
-#
-# Copyright (c) 2014 10X Genomics, Inc. All rights reserved.
-#
